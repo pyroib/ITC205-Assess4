241ec90c7595e1a3f3cd0a8380805e1639efa744
Ian B <github@blott.com.au>
2018-10-09 22:26:49 +1100

    junit testing bug #2

--- src/hotel/entities/Hotel.java
+++ src/hotel/entities/Hotel.java
@@ -125,7 +125,7 @@ public class Hotel {
 			String mesg = String.format("Hotel: checkout: no booking present for room id : %d", roomId);
 			throw new RuntimeException(mesg);
 		} else {
-	//		activeBookingsByRoomId.remove(roomId);
+			activeBookingsByRoomId.remove(roomId);
 			booking.checkOut();
 		}
 	}
